3c3ce7f52e4f1c496545b46d3d297b1c1f4c4d73
lokmanhossen678 <lokmanhossen678@gmail.com>
2025-04-14 19:13:33 +0600

    added aside class

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -4,7 +4,11 @@ const SideBar = () => {
     return (
         <aside>
             <nav>
-                
+                <p>Side Nav -1</p>
+                <p>Side Nav -2</p>
+                <p>Side Nav -3</p>
+                <p>Side Nav -4</p>
+                <p>Side Nav -5</p>
             </nav>
         </aside>
     );
