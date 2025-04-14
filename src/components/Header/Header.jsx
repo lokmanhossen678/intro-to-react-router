67f660623109ebffb4d4f062fcaa1b533a96281e
lokmanhossen678 <lokmanhossen678@gmail.com>
2025-04-14 17:58:56 +0600

    added angcor

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const Header = () => {
-    return (
-        <div>
-            <h3>This is header</h3>
-        </div>
-    );
+  return (
+    <div>
+      <h3>This is header</h3>
+      <nav>
+        <a href="/">Home</a>
+        <a href="/Mobils">Mobils</a>
+        <a href="/laptops">Laptops</a>
+        <a href="/"></a>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
